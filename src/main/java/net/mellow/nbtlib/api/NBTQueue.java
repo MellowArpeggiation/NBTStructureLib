8b0b0533ab28cdc009dde6588321efdfa458c01c
George Paton <georgewpaton@me.com>
2025-12-09 16:45:18 +1100

    serialize queued postgen into the chunk in the corner of the queued structure

--- src/main/java/net/mellow/nbtlib/api/NBTQueue.java
+++ src/main/java/net/mellow/nbtlib/api/NBTQueue.java
@@ -2,6 +2,7 @@ package net.mellow.nbtlib.api;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
@@ -11,10 +12,16 @@ import cpw.mods.fml.common.gameevent.TickEvent;
 import net.mellow.nbtlib.Config;
 import net.mellow.nbtlib.Registry;
 import net.mellow.nbtlib.api.NBTStructure.JigsawConnection;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.gen.structure.StructureBoundingBox;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.util.Constants;
 import net.minecraftforge.common.util.ForgeDirection;
+import net.minecraftforge.event.world.ChunkDataEvent;
+import net.minecraftforge.event.world.ChunkEvent;
 import net.minecraftforge.event.world.WorldEvent;
 
 public class NBTQueue {
@@ -48,15 +55,19 @@ public class NBTQueue {
 
         public final StructureBoundingBox boundingBox;
 
+        public final long seed;
+
         public boolean hasBuilt;
 
-        private Tandem(String spawnName, JigsawPiece piece, int x, int y, int z, int coordBaseMode) {
+        private Tandem(String spawnName, JigsawPiece piece, int x, int y, int z, int coordBaseMode, long seed) {
             this.spawnName = spawnName;
             this.piece = piece;
 
             this.coordBaseMode = coordBaseMode;
 
-            switch (this.coordBaseMode) {
+            this.seed = seed;
+
+            switch (coordBaseMode) {
             case 1:
             case 3:
                 this.boundingBox = new StructureBoundingBox(x, y, z, x + piece.structure.size.z - 1, y + piece.structure.size.y - 1, z + piece.structure.size.x - 1);
@@ -65,6 +76,17 @@ public class NBTQueue {
                 this.boundingBox = new StructureBoundingBox(x, y, z, x + piece.structure.size.x - 1, y + piece.structure.size.y - 1, z + piece.structure.size.z - 1);
                 break;
             }
+        }
+
+        private Tandem(String spawnName, String jigsawName, int x1, int y1, int z1, int x2, int y2, int z2, int coordBaseMode, long seed) {
+            this.spawnName = spawnName;
+            this.piece = JigsawPiece.jigsawMap.get(jigsawName);
+
+            this.coordBaseMode = coordBaseMode;
+
+            this.boundingBox = new StructureBoundingBox(x1, y1, z1, x2, y2, z2);
+
+            this.seed = seed;
         }
 
         public static Tandem selectPiece(String spawnName, String poolName, String targetName, Random rand, int x, int y, int z, ForgeDirection dir) {
@@ -87,21 +109,23 @@ public class NBTQueue {
             int oy = toConnection.pos.y;
             int oz = nextPiece.structure.rotateZ(toConnection.pos.x, toConnection.pos.z, nextCoordBase);
 
-            return new Tandem(spawnName, nextPiece, x - ox + dir.offsetX, y - oy + dir.offsetY, z - oz + dir.offsetZ, nextCoordBase);
+            return new Tandem(spawnName, nextPiece, x - ox + dir.offsetX, y - oy + dir.offsetY, z - oz + dir.offsetZ, nextCoordBase, rand.nextLong());
         }
 
         private static int directionOffsetToCoordBase(ForgeDirection from, ForgeDirection to) {
-            for(int i = 0; i < 4; i++) {
-                if(from == to) return i % 4;
+            for (int i = 0; i < 4; i++) {
+                if (from == to) return i % 4;
                 from = from.getRotation(ForgeDirection.DOWN);
             }
             return 0;
         }
 
-        public void attemptBuild(World world, Random rand) {
+        public void attemptBuild(World world) {
             if (hasBuilt) return;
 
             if (!world.checkChunksExist(boundingBox.minX, boundingBox.minY, boundingBox.minZ, boundingBox.maxX, boundingBox.maxY, boundingBox.maxZ)) return;
+
+            Random rand = new Random(seed);
 
             if (Config.debugSpawning) {
                 Registry.LOG.info("[Tandem] building: " + piece.name + " at " + boundingBox.toString());
@@ -111,6 +135,39 @@ public class NBTQueue {
             piece.structure.build(world, rand, spawnName, piece, boundingBox, boundingBox, coordBaseMode, 2);
 
             hasBuilt = true;
+        }
+
+        // should this tandem be saved into this chunk
+        public boolean isStoredInChunk(Chunk chunk) {
+            return boundingBox.minX >> 4 == chunk.xPosition && boundingBox.minZ >> 4 == chunk.zPosition;
+        }
+
+        public void writeToNBT(NBTTagCompound nbt) {
+            nbt.setString("spawn", spawnName);
+            nbt.setString("jigsaw", piece.name);
+            nbt.setInteger("x1", boundingBox.minX);
+            nbt.setInteger("y1", boundingBox.minY);
+            nbt.setInteger("z1", boundingBox.minZ);
+            nbt.setInteger("x2", boundingBox.maxX);
+            nbt.setInteger("y2", boundingBox.maxY);
+            nbt.setInteger("z2", boundingBox.maxZ);
+            nbt.setByte("rot", (byte)coordBaseMode);
+            nbt.setLong("seed", seed);
+        }
+
+        public static Tandem readFromNBT(NBTTagCompound nbt) {
+            return new Tandem(
+                nbt.getString("spawn"),
+                nbt.getString("jigsaw"),
+                nbt.getInteger("x1"),
+                nbt.getInteger("y1"),
+                nbt.getInteger("z1"),
+                nbt.getInteger("x2"),
+                nbt.getInteger("y2"),
+                nbt.getInteger("z2"),
+                nbt.getByte("rot"),
+                nbt.getLong("seed")
+            );
         }
 
     }
@@ -123,8 +180,6 @@ public class NBTQueue {
 
             NBTQueue queue = dimensionMap.get(event.world.provider.dimensionId);
 
-            Random rand = new Random(event.world.getSeed());
-
             // we mark the piece as built and remove them later, because pieces can add MORE pieces,
             // causing a ConcurrentModificationException
 
@@ -133,10 +188,66 @@ public class NBTQueue {
 
             int count = queue.tandemQueue.size();
             for (int i = 0; i < count; i++) {
-                queue.tandemQueue.get(i).attemptBuild(event.world, rand);
+                queue.tandemQueue.get(i).attemptBuild(event.world);
             }
 
             queue.tandemQueue.removeIf(tandem -> tandem.hasBuilt);
+        }
+
+        @SubscribeEvent
+        public void onChunkLoad(ChunkDataEvent.Load event) {
+            NBTQueue queue = dimensionMap.get(event.world.provider.dimensionId);
+
+            NBTTagCompound nbt = event.getData();
+
+            if (nbt.hasKey("NBTQueue")) {
+                NBTTagList list = nbt.getTagList("NBTQueue", Constants.NBT.TAG_COMPOUND);
+
+                int count = list.tagCount();
+                for (int i = 0; i < count; i++) {
+                    NBTTagCompound tandemData = list.getCompoundTagAt(i);
+                    queue.tandemQueue.add(Tandem.readFromNBT(tandemData));
+                }
+            }
+        }
+
+        @SubscribeEvent
+        public void onChunkSave(ChunkDataEvent.Save event) {
+            NBTQueue queue = dimensionMap.get(event.world.provider.dimensionId);
+
+            Chunk chunk = event.getChunk();
+            NBTTagCompound nbt = event.getData();
+
+            NBTTagList list = new NBTTagList();
+
+            Iterator<Tandem> iter = queue.tandemQueue.iterator();
+            while (iter.hasNext()) {
+                Tandem tandem = iter.next();
+                if (tandem.isStoredInChunk(chunk)) {
+                    NBTTagCompound tandemData = new NBTTagCompound();
+                    tandem.writeToNBT(tandemData);
+                    list.appendTag(tandemData);
+                }
+            }
+
+            if (list.tagCount() > 0) {
+                nbt.setTag("NBTQueue", list);
+            }
+        }
+
+        @SubscribeEvent
+        public void onChunkUnload(ChunkEvent.Unload event) {
+            NBTQueue queue = dimensionMap.get(event.world.provider.dimensionId);
+
+            Chunk chunk = event.getChunk();
+
+            Iterator<Tandem> iter = queue.tandemQueue.iterator();
+            while (iter.hasNext()) {
+                Tandem tandem = iter.next();
+                if (tandem.isStoredInChunk(chunk)) {
+                    iter.remove();
+                }
+            }
         }
 
         @SubscribeEvent
